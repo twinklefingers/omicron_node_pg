05545cad129b6c3fa6508e4565586a1272fbaf52
twinklefingers <B1-66-ER@Rays-MacBook-Pro.local>
2016-08-16 11:30:40 -0500

    continuing work

--- server/routes/books.js
+++ server/routes/books.js
@@ -1,10 +1,35 @@
 var express = require('express');
 var router = express.Router();
+var pg = require('pg');
+var connectionString = 'postgres://localhost:5432/omicron'; //case sensitive
+//5432 default port of PG databases
 
 router.get('/', function(req, res) {
     // Retrieve books from database
-    res.send({
-        message: 'OK'
+    pg.connect(connectionString, function(err, client, done) {
+
+        if (err) {
+            res.sendStatus(500);
+        }
+
+        client.query('SELECT * FROM books', function(err, result) { //second param always holds results, no matter what it's called
+            done(); //only 10 query connections can be open at once, so call done or the connections will stay open and the 11th query wont work
+            if (err) {
+                res.sendStatus(500);
+            }
+            res.send(result.rows);
+        });
+    });
+});
+
+router.post('/', function(req, res) {
+    var book = req.body;
+    pg.connect(connectionString, function(err, client, done) {
+        if (err) {
+            res.sendStatus(500);
+        }
+        client.quert('INSERT INTO books (author, title, published)' +
+            'VALUES ($1, $2, $3)');
     });
 });
 
